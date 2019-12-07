e1c5653dfaec3e5906008754dc609c655e2b6f23
taylor <zmq199994@163.com>
2019-12-07 18:17:16 +0800

    托尔斯泰

--- 002_add_two_numbers/add_two_numbers.c
+++ 002_add_two_numbers/add_two_numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#第二次测试
 /* Definition for singly-linked list. */
-struct ListNode {
+struct ListNode
+{
     int val;
     struct ListNode *next;
 };
 
-static struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+static struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
     int carry_num = 0;
     int first = 1;
@@ -16,37 +17,46 @@ static struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
     struct ListNode *p = NULL;
     struct ListNode *prev = p;
 
-    while (l1 != NULL || l2 != NULL || carry_num) {
+    while (l1 != NULL || l2 != NULL || carry_num)
+    {
         int sum = 0;
         int last_carry = carry_num;
-        if (l1 != NULL) {
+        if (l1 != NULL)
+        {
             sum += l1->val;
             l1 = l1->next;
         }
-        if (l2 != NULL) {
+        if (l2 != NULL)
+        {
             sum += l2->val;
             l2 = l2->next;
         }
-        if (sum >= 10) {
+        if (sum >= 10)
+        {
             sum -= 10;
             carry_num = 1;
-        } else {
+        }
+        else
+        {
             carry_num = 0;
         }
 
         p = malloc(sizeof(*p));
-        if (first) {
-             res = p;
-             first = 0;
+        if (first)
+        {
+            res = p;
+            first = 0;
         }
         p->val = sum + last_carry;
-        if (p->val >= 10) {
+        if (p->val >= 10)
+        {
             p->val -= 10;
             carry_num = 1;
         }
         p->next = NULL;
-        if (prev != NULL) {
-             prev->next = p;
+        if (prev != NULL)
+        {
+            prev->next = p;
         }
         prev = p;
     }
@@ -61,15 +71,18 @@ static struct ListNode *node_build(const char *digits)
     int len = strlen(digits);
     const char *c = digits + len - 1;
     prev = NULL;
-    while (len-- > 0) {
+    while (len-- > 0)
+    {
         p = malloc(sizeof(*p));
-        if (first) {
+        if (first)
+        {
             first = 0;
             res = p;
         }
         p->val = *c-- - '0';
         p->next = NULL;
-        if (prev != NULL) {
+        if (prev != NULL)
+        {
             prev->next = p;
         }
         prev = p;
@@ -81,7 +94,8 @@ static struct ListNode *node_build(const char *digits)
 static void show(struct ListNode *ln)
 {
     int sum = 0, factor = 1;
-    while (ln != NULL) {
+    while (ln != NULL)
+    {
         sum += ln->val * factor;
         factor *= 10;
         ln = ln->next;
@@ -91,7 +105,8 @@ static void show(struct ListNode *ln)
 
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr, "Usage: ./test n1 n2\n");
         exit(-1);
     }
